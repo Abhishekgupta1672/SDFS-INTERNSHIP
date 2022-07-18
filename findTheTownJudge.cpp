d7fab01f0219072fe4e75d42f3b83c73bf0a61e7
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-18 22:21:38 +0530

    Create findTheTownJudge.cpp

---
+++ findTheTownJudge.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.empty() && n==1) return 1;
+        vector<int>out(n+1,0);
+        vector<int>in(n+1,0);
+        for(auto x:trust)
+        {
+            out[x[0]]++;
+            in[x[1]]++;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            if(in[i]==n-1 && out[i]==0)
+                return i;
+        }
+        return -1;
+    }
+};
